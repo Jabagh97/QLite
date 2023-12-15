57c9531804505ce7940b12b5b6f1429d01994ce6
jabagh97 <jabagh.anjouk@ozu.edu.tr>
2023-12-15 17:52:08 +0300

    Start

---
+++ PortalPOC/Controllers/GenericTableController .cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortalPOC.Context;
+using PortalPOC.Models;
+using System.Data.Entity;
+
+namespace PortalPOC.Controllers
+{
+    public class GenericTableController : Controller
+    {
+        private readonly QuavisQorchAdminEasyTestContext _dbContext;
+
+        // Define a dictionary to map model names to types
+        private readonly Dictionary<string, Type> modelTypeMapping = new Dictionary<string, Type>
+    {
+        {"Branch", typeof(Branch)}, 
+    };
+
+        public GenericTableController(QuavisQorchAdminEasyTestContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index(string modelName)
+        {
+            if (!modelTypeMapping.TryGetValue(modelName, out Type modelType))
+            {
+                // Handle the case when the modelName is not found in the mapping
+                return NotFound();
+            }
+
+            return View(modelType);
+        }
+    }
+
+}
