32aa7068b170c0f91bb2b589bfc6ce256bf23290
Jabağ Anjouk <76058182+Jabagh97@users.noreply.github.com>
2024-01-02 22:31:44 +0200

    warmup

--- PortalPOC/Program.cs
+++ PortalPOC/Program.cs
@@ -9,6 +9,10 @@ using PortalPOC.Services;
 using PortalPOC.Helpers;
 using PortalPOC.QueryFactory;
 using System.Reflection;
+using System.Linq.Dynamic.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using PortalPOC.Controllers;
 
 internal class Program
 {
@@ -31,11 +35,32 @@ internal class Program
         builder.Services.AddScoped<IModelTypeMappingService, ModelTypeMappingService>();
         builder.Services.AddScoped<IDataTableRequestExtractor, DataTableRequestExtractor>();
         builder.Services.AddScoped<IQueryFactory, QueryFactory>();
+        builder.Logging.AddConsole();
+        builder.Logging.AddDebug();
 
         var app = builder.Build();
 
         AppDomain.CurrentDomain.AssemblyLoad += CurrentDomain_AssemblyLoad;
 
+
+
+        // Perform warm-up activities
+        using (var scope = app.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            try
+            {
+                var dbContext = services.GetRequiredService<QuavisQorchAdminEasyTestContext>();
+                WarmUpEntity<KappRole>(dbContext);
+
+                Console.WriteLine("Warming Up Done !!!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred during warm-up.");
+                Console.WriteLine(ex.Message);
+            }
+        }
 
         // Initialize the database if InitDatabase is true
         var initDatabase = Convert.ToBoolean(app.Configuration["InitDatabase"]);
@@ -82,6 +107,19 @@ internal class Program
 
         app.Run();
     }
+    private static void WarmUpEntity<TEntity>(QuavisQorchAdminEasyTestContext dbContext) where TEntity : class
+    {
+        // Use LINQ dynamic to perform a query on the DbSet<TEntity>
+        var query = dbContext.Set<TEntity>().AsQueryable();
+
+        // Example: Retrieve the first record
+        var firstRecord = query.FirstOrDefault();
+
+        var filteredData = query.Where("Gcrecord == 1").ToList();
+
+        // You can perform additional operations on the first record or customize the query as needed
+        // For example: 
+    }
     private static void CurrentDomain_AssemblyLoad(object sender, AssemblyLoadEventArgs args)
     {
         // Handle the assembly load event
@@ -90,18 +128,7 @@ internal class Program
     }
 
 
-    private static void LoadAssembly(string assemblyName)
-    {
-        try
-        {
-            // Try loading the assembly
-            Assembly.Load(assemblyName);
-            Console.WriteLine($"Assembly loaded: {assemblyName}");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error loading assembly {assemblyName}: {ex.Message}");
-        }
-    }
+
+
 
 }
