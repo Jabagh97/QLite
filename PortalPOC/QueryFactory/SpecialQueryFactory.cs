8a7cf89879b3ff2c21660e2c107c83e44754fc85
Jabağ Anjouk <76058182+Jabagh97@users.noreply.github.com>
2024-01-03 22:18:14 +0200

    561

---
+++ PortalPOC/QueryFactory/SpecialQueryFactory.cs
@@ -0,0 +1,38 @@
+﻿using PortalPOC.Models;
+
+namespace PortalPOC.QueryFactory
+{
+    public interface ISpecialQueryFactory : IQueryFactory
+    {
+        // Add additional methods specific to the special table
+    }
+
+    public class SpecialQueryFactory : ISpecialQueryFactory
+    {
+        // Implement the specialized methods
+        public void CreateInstance(QuavisQorchAdminEasyTestContext _dbContext, object modelInstance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetById(string entityId, IQueryable data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable SelectAndJoinQuery(IQueryable? data, Type modelType, Type viewModelType, QuavisQorchAdminEasyTestContext dbContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SoftDeleteInstance(QuavisQorchAdminEasyTestContext _dbContext, IQueryable data, Type modelType, string Oid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateInstance(QuavisQorchAdminEasyTestContext _dbContext, object existingEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
