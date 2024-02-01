e0dc22498bd3fcafdefd10bd4ffb4c93d4f5c431
jabagh97 <jabagh.anjouk@ozu.edu.tr>
2024-02-01 14:41:30 +0300

    .

---
+++ AdminPortal/Constants/EndPoints.cs
@@ -0,0 +1,20 @@
+﻿namespace AdminPortal.Constants
+{
+    public class EndPoints
+    {
+        public static string AdminGetData(string modelName, string searchValue, string sortColumn, string sortColumnDirection, int skip, int pageSize) =>
+    $"api/Admin/GetData?modelName={modelName}&searchValue={searchValue}&sortColumn={sortColumn}&sortColumnDirection={sortColumnDirection}&skip={skip}&pageSize={pageSize}";
+
+
+        public static string GetdropDowns(string modelName) =>
+  $"api/Admin/GetDropDowns?modelName={modelName}";
+
+
+        public static string AdminDelete = "api/Admin/Delete";
+        public static string AdminCreate = "api/Admin/Create";
+
+        public static string AdminEdit = "api/Admin/Edit";
+
+
+    }
+}
--- AdminPortal/Controllers/GenericTableController .cs
+++ AdminPortal/Controllers/GenericTableController .cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdminPortal.Constants;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.SqlServer.Server;
 using Newtonsoft.Json;
@@ -17,16 +18,13 @@ namespace PortalPOC.Controllers
     {
         private readonly IModelTypeMappingService _modelTypeMappingService;
         private readonly IDataTableRequestExtractor _requestExtractor;
-
         private readonly HttpClient _httpClient;
-
 
         public GenericTableController(IModelTypeMappingService modelTypeMappingService, IDataTableRequestExtractor requestExtractor)
         {
             _modelTypeMappingService = modelTypeMappingService;
             _requestExtractor = requestExtractor;
-            _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri("http://localhost:5169/");
+            _httpClient = new HttpClient { BaseAddress = new Uri("http://localhost:5169/") };
         }
 
         public IActionResult Index(string modelName)
@@ -37,8 +35,6 @@ namespace PortalPOC.Controllers
             }
 
             ViewBag.PageTitle = modelName;
-
-
             return View(ViewNavigations.GenericTable, viewModelType);
         }
 
@@ -50,181 +46,48 @@ namespace PortalPOC.Controllers
             {
                 var parameters = _requestExtractor.ExtractParameters(Request.Form);
 
-                var apiUrl = $"api/Admin/GetData?modelName={modelName}&searchValue={parameters.SearchValue}&sortColumn={parameters.SortColumn}&sortColumnDirection={parameters.SortColumnDirection}&skip={parameters.Skip}&pageSize={parameters.PageSize}";
-
-                var response = await _httpClient.GetAsync(apiUrl);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var jsonData = await response.Content.ReadAsStringAsync();
-                    return Ok(jsonData);
-                }
-                else
-                {
-                    return StatusCode(500, new { success = false, message = "Internal Server Error" });
-                }
+                var response = await _httpClient.GetAsync(EndPoints.AdminGetData(modelName, parameters.SearchValue, parameters.SortColumn, parameters.SortColumnDirection, parameters.Skip, parameters.PageSize));
+                return response.IsSuccessStatusCode
+                    ? Ok(await response.Content.ReadAsStringAsync())
+                    : StatusCode(500, new { success = false, message = "Internal Server Error" });
             }
             catch (Exception ex)
             {
-                return StatusCode(500, new { success = false, message = ex.Message + " Internal Server Error" });
+                return StatusCode(500, new { success = false, message = $"{ex.Message} Internal Server Error" });
             }
         }
-
-
 
         [HttpPost]
-        public IActionResult Create([FromBody] Dictionary<string, object> formData)
-        {
-            try
-            {
-                var apiUrl = "api/Admin/Create";
-
-
-                var convertedFormData = formData.ToDictionary(
-                    kvp => kvp.Key,
-                    kvp => Utils.ConvertJsonElementValue((JsonElement)kvp.Value));
-
-                var jsonFormData = JsonConvert.SerializeObject(convertedFormData);
-
-
-                // Create HttpContent with JSON data
-                var content = new StringContent(jsonFormData, Encoding.UTF8, "application/json");
-
-                // Make the request to the second API method
-                var response = _httpClient.PostAsync(apiUrl, content).Result;
-
-                if (response.IsSuccessStatusCode)
-                {
-                    // Process the response if needed
-                    var result = response.Content.ReadAsStringAsync().Result;
-                    return Ok(result);
-                }
-                else
-                {
-                    return StatusCode((int)response.StatusCode, new { success = false, message = "Error creating" });
-                }
-
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new { success = false, message = ex.Message });
-            }
-        }
-      
-
-
-
+        public IActionResult Create([FromBody] Dictionary<string, object> formData) =>
+            ProcessData(EndPoints.AdminCreate, formData, "Item created successfully", "Error creating");
 
         [HttpPost]
-        public IActionResult Edit([FromBody] Dictionary<string, object> formData)
-        {
-            try
-            {
-                try
-                {
-                    var apiUrl = "api/Admin/Edit";
-
-
-                    var convertedFormData = formData.ToDictionary(
-                        kvp => kvp.Key,
-                        kvp => Utils.ConvertJsonElementValue((JsonElement)kvp.Value));
-
-                    var jsonFormData = JsonConvert.SerializeObject(convertedFormData);
-
-
-                    // Create HttpContent with JSON data
-                    var content = new StringContent(jsonFormData, Encoding.UTF8, "application/json");
-
-                    // Make the request to the second API method
-                    var response = _httpClient.PostAsync(apiUrl, content).Result;
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        // Process the response if needed
-                        var result = response.Content.ReadAsStringAsync().Result;
-                        return Ok(result);
-                    }
-                    else
-                    {
-                        return StatusCode((int)response.StatusCode, new { success = false, message = "Error Editing" });
-                    }
-
-                }
-                catch (Exception ex)
-                {
-                    return StatusCode(500, new { success = false, message = ex.Message });
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new { success = false, message = ex.Message });
-            }
-        }
-
+        public IActionResult Edit([FromBody] Dictionary<string, object> formData) =>
+            ProcessData(EndPoints.AdminEdit, formData, "Item edited successfully", "Error editing");
 
         [HttpPost]
-        public IActionResult Delete([FromBody] Dictionary<string, object> Data)
-        {
-            try
-            {
-                var apiUrl = "api/Admin/Delete";
-
-
-                var convertedFormData = Data.ToDictionary(
-                    kvp => kvp.Key,
-                    kvp => Utils.ConvertJsonElementValue((JsonElement)kvp.Value));
-
-                var jsonFormData = JsonConvert.SerializeObject(convertedFormData);
-
-
-                // Create HttpContent with JSON data
-                var content = new StringContent(jsonFormData, Encoding.UTF8, "application/json");
-
-                // Make the request to the second API method
-                var response = _httpClient.PostAsync(apiUrl, content).Result;
-
-
-                // Return a success response with the created model
-                return Ok(new { success = true, message = "item Deleted successfully" });
-            }
-            catch (Exception ex)
-            {
-                // Handle exceptions and return an error response
-                return StatusCode(500, new { success = false, message = ex.Message });
-            }
-        }
-
+        public IActionResult Delete([FromBody] Dictionary<string, object> formData) =>
+            ProcessData(EndPoints.AdminDelete, formData, "Item deleted successfully", "Error deleting");
 
         public async Task<IActionResult> ShowPopup(string modelName, string opType, Dictionary<string, string> data)
         {
             try
             {
-                var modelTypeMapping = _modelTypeMappingService.GetModelTypeMapping();
-
-
                 if (!_modelTypeMappingService.TryGetModelTypes(modelName, out var modelType, out var viewModelType))
                 {
                     return NotFound(Errors.NullModel);
                 }
 
-
-                var apiUrl = $"api/Admin/GetDropDowns?modelName={modelName}";
-
-                var response = await _httpClient.GetAsync(apiUrl);
+                var response = await _httpClient.GetAsync(EndPoints.GetdropDowns(modelName));
 
                 if (response.IsSuccessStatusCode)
                 {
-                    var namesDictionaryJson = await response.Content.ReadAsStringAsync();
-                    var namesDictionary = JsonConvert.DeserializeObject<Dictionary<string, List<dynamic>>>(namesDictionaryJson);
-
-                    ViewBag.DropDowns = namesDictionary;
-
+                    ViewBag.DropDowns = JsonConvert.DeserializeObject<Dictionary<string, List<dynamic>>>(await response.Content.ReadAsStringAsync());
                 }
                 else
                 {
                     return StatusCode((int)response.StatusCode, new { success = false, message = "PopUp Error" });
                 }
-
 
                 if (!opType.IsNullOrEmpty() && opType.Contains(Operations.Edit))
                 {
@@ -234,17 +97,38 @@ namespace PortalPOC.Controllers
                 ViewBag.ViewModel = viewModelType;
                 ViewBag.Action = opType;
 
-
                 return PartialView(ViewNavigations.GenericPartial, modelType);
-
             }
             catch (Exception ex)
             {
-                // Log the exception
                 return NotFound($" '{modelName}' {Errors.ModelNotFound}  {ex.Message}");
             }
         }
 
+
+
+        public IActionResult ProcessData(string apiUrl, Dictionary<string, object> formData, string successMessage, string errorMessage)
+        {
+            try
+            {
+                var convertedFormData = formData.ToDictionary(
+                    kvp => kvp.Key,
+                    kvp => Utils.ConvertJsonElementValue((JsonElement)kvp.Value));
+
+                var jsonFormData = JsonConvert.SerializeObject(convertedFormData);
+
+                var content = new StringContent(jsonFormData, Encoding.UTF8, "application/json");
+                var response = _httpClient.PostAsync(apiUrl, content).Result;
+
+                return response.IsSuccessStatusCode
+                    ? Ok(new { success = true, message = successMessage })
+                    : StatusCode((int)response.StatusCode, new { success = false, message = errorMessage });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { success = false, message = ex.Message });
+            }
+        }
 
 
     }
