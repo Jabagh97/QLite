84dc7dcbb3e0795225333bc320b9ed6d960f396e
jabagh97 <jabagh.anjouk@ozu.edu.tr>
2024-04-06 16:05:57 +0300

    0

---
+++ QLite.Data/Models/Device.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static QLite.Data.Models.Enums;
+
+namespace QLite.Data
+{
+    public class Device
+    {
+
+        public Guid Oid { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime CreatedDateUtc { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+        public DateTime? ModifiedDateUtc { get; set; }
+
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+
+
+        public string Name { get; set; }
+
+        public Guid Kiosk { get; set; }
+
+        public QLDevice DeviceType { get; set; }
+
+        public string Settings { get; set; }
+
+        public int? Gcrecord { get; set; }
+
+        public virtual ICollection<Kiosk> Kiosks { get; } = new List<Kiosk>();
+
+    }
+}
